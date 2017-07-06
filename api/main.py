b8502d72e683c76a501f8df1691590cc9bc1f524
mercurycarver@gmail.com <mercurycarver@gmail.com>
2017-07-06 17:06:40 -0400

    tried to make changes

--- api/main.py
+++ api/main.py
@@ -3,20 +3,26 @@ import json
 import random
 import urllib2
 import urllib
+import jinja2
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader("templates"))
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         base_url = "http://api.giphy.com/v1/gifs/search?"
         url_params = {'q': 'puppy', 'api_key': 'dc6zaTOxFJmzC', 'limit': 10}
-        #giphy_response = urllib2.urlopen(
+        template = jinja_environment.get_template('api.html')#giphy_response = urllib2.urlopen(
             #"http://api.giphy.com/v1/gifs/search?q=+ryan+gosling&api_key=dc6zaTOxFJmzC&limit=10")
         giphy_response = urllib2.urlopen(base_url + urllib.urlencode(url_params)).read()
         parsed_giphy_dictionary = json.loads(giphy_response)
         gif_url = parsed_giphy_dictionary['data'][0]['images']['original']['url']
-        self.response.write(gif_url)
+        app = webapp2.WSGIApplication
+        self.response.out.write(template.render(dictionary))
 
-class SecondHandler(webapp2.RequestHandlers):
-    def get
+#class SecondHandler(webapp2.RequestHandlers):
+    #def get
 
 app = webapp2.WSGIApplication([
      ('/', MainHandler)
